0a30681f32d8dc789ae730b110bd65e8c9b7fc8d
vantruong <phantryong@gmail.com>
2024-06-05 12:52:17 +0700

    www

--- FrontEnd/vite-project/src/App.jsx
+++ FrontEnd/vite-project/src/App.jsx
@@ -23,6 +23,7 @@ function App() {
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
+          hihihi
         </p>
       </div>
       <p className="read-the-docs">
