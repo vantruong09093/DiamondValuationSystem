9e1d84c73210a0e279eb0ea0afd53cdb166b319a
zunguyen1505@gmail.com <zunguyen1505@gmail.com>
2024-06-05 13:51:10 +0700

    nguyen update

--- BackEnd/src/index.js
+++ BackEnd/src/index.js
@@ -2,8 +2,7 @@ import express from "express";
 const app = express();
 const port = 3000;
 app.get("/", (req, res) => {
-  res.send("hello world little boy");
-
+  res.send("hello world nguyen");
 });
 
 app.listen(port, () => {
