a04ef615c7ef8bd4702c5e00ac76d4259f25827e
ang <tuanan298@icloud.com>
2024-06-11 20:30:44 +0700

    chore: Add AuthProvider and Login component for authentication

--- FrontEnd/vite-project/src/App.jsx
+++ FrontEnd/vite-project/src/App.jsx
@@ -1,14 +1,16 @@
 
 import {Routes, Route, BrowserRouter as Router} from 'react-router-dom'
-
-
+import { AuthProvider } from './Context/AuthContext'
+import Login from './Views/Login'
 function App() {
 
 
   return (
     <Router>
       <Routes>
-        
+        <AuthProvider>
+          <Route path="/" element={<Login />} />
+        </AuthProvider>
       </Routes>
     </Router>
   )
