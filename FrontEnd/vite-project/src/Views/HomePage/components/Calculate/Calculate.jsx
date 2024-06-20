285e4329b2abebdb24597013debeba83d8da5eab
ang <tuanan298@icloud.com>
2024-06-20 22:13:07 +0700

    remove unuse import

--- FrontEnd/vite-project/src/Views/HomePage/components/Calculate/Calculate.jsx
+++ FrontEnd/vite-project/src/Views/HomePage/components/Calculate/Calculate.jsx
@@ -1,7 +1,5 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import Button from "react-bootstrap/Button";
-import Headers from "../../components/Headers/Headers";
+import { useState } from "react";
+
 import Navbar from "../../components/Navbar/Navbar";
 import CalculateContent from "../CalculateContext/CalculateContent";
 function Calculate() {
