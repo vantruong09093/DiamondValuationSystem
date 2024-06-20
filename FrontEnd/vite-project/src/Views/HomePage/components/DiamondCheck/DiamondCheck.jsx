78b827f507821580644ec6ff863852373657b411
ang <tuanan298@icloud.com>
2024-06-20 19:22:09 +0700

    removing un use import

--- FrontEnd/vite-project/src/Views/HomePage/components/DiamondCheck/DiamondCheck.jsx
+++ FrontEnd/vite-project/src/Views/HomePage/components/DiamondCheck/DiamondCheck.jsx
@@ -1,11 +1,7 @@
-import React, { useContext, useState } from "react";
-import { Link } from "react-router-dom";
-import Button from "react-bootstrap/Button";
-import Headers from "../../components/Headers/Headers";
+import  { useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import CertificateComponent from "../../components/CertificateComponent/CertificateComponent";
-import Footer from "../../components/Footer/Footer";
-import { UserContext } from "../../components/CalculateContext/CalculateContent";
+
 
 function DiamondCheck() {
   const [menu, setMenu] = useState("diamondcheck");
