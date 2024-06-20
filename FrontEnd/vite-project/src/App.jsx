3f9b00fa6d2d278be4fabede00a85a3305174d5e
ang <tuanan298@icloud.com>
2024-06-20 15:55:16 +0700

    just ignor ethis

--- FrontEnd/vite-project/src/App.jsx
+++ FrontEnd/vite-project/src/App.jsx
@@ -1,17 +1,20 @@
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import { AuthProvider } from "./Context/AuthContext";
 import { useState } from "react";
-
+import Home from "./Views/HomePage/components/Home/Home";
 import DashboardRoutes from "../src/Routes/Dashboard";
-
+import Layout from "./Views/HomePage/components/Layout/Layout";
 function App() {
   const [isSidebar, setIsSidebar] = useState(true);
-// private route later
+  // private route later
   return (
     <AuthProvider>
       <Router>
         <Routes>
           <Route path="/dashboard/*" element={<DashboardRoutes isSidebar={isSidebar} setIsSidebar={setIsSidebar} />} />
+          <Route path="/" element={<Layout />} />
+          <Route index element={<Home />} />
+          <Route path="home" element={<Home />} />
         </Routes>
       </Router>
     </AuthProvider>
