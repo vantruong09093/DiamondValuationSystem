a1ca1841c56f317570c4c5c6a5608a6a61f2b83a
ang <tuanan298@icloud.com>
2024-06-20 18:10:29 +0700

    feat: Update DashboardLayout to use CSS modules

--- FrontEnd/vite-project/src/Views/dashboard/Layout/DashboardLayout.jsx
+++ FrontEnd/vite-project/src/Views/dashboard/Layout/DashboardLayout.jsx
@@ -3,7 +3,7 @@ import Sidebar from "../../dashboard/scenes/global/Sidebar";
 import Topbar from "../../dashboard/scenes/global/Topbar";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "../theme";
-
+import styles from "../../../index.module.css";
 const DashboardLayout = ({ isSidebar, setIsSidebar }) => {
   const [theme, colorMode] = useMode();
 
@@ -11,9 +11,9 @@ const DashboardLayout = ({ isSidebar, setIsSidebar }) => {
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <div className="app">
+        <div className={styles.app}>
           <Sidebar isSidebar={isSidebar} />
-          <main className="content">
+          <main className={styles.content}>
             <Topbar setIsSidebar={setIsSidebar} />
             <Outlet />
           </main>
