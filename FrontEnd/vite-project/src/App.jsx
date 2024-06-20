ec3ec8c37c22253525d521b91de7e58920a39e9d
ang <tuanan298@icloud.com>
2024-06-20 21:34:51 +0700

    move stuff around

--- FrontEnd/vite-project/src/App.jsx
+++ FrontEnd/vite-project/src/App.jsx
@@ -1,18 +1,18 @@
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import { AuthProvider } from "./Context/AuthContext";
-import { useState } from "react";
+
 import DashboardRoutes from "../src/Routes/Dashboard";
 import HomeRoutes from "../src/Routes/Home";
 import Footer from "./Views/HomePage/components/Footer/Footer";
 
 function App() {
-  const [isSidebar, setIsSidebar] = useState(true);
+
   // private route later
   return (
     <AuthProvider>
       <Router>
         <Routes>
-          <Route path="/dashboard/*" element={<DashboardRoutes isSidebar={isSidebar} setIsSidebar={setIsSidebar} />} />
+          <Route path="/dashboard/*" element={<DashboardRoutes/>} />
           <Route path="/*" element={<HomeRoutes />} />
         </Routes>
       </Router>
