d952d87d6d9d98d0002a93e33825192702658581
ang <tuanan298@icloud.com>
2024-06-22 02:06:31 +0700

    add notify provider và add nó vào app

--- FrontEnd/vite-project/src/App.jsx
+++ FrontEnd/vite-project/src/App.jsx
@@ -1,22 +1,25 @@
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import { AuthProvider } from "./Context/AuthContext";
-
 import DashboardRoutes from "../src/Routes/Dashboard";
 import HomeRoutes from "../src/Routes/Home";
 import Footer from "./Views/HomePage/components/Footer/Footer";
 import PrivateRoute from "./Routes/PrivateRoute";
+import { NotifyProvider } from "./Provider/NotifyProvider";
+import "react-toastify/dist/ReactToastify.css";
 function App() {
   // private route later
   return (
     <Router>
       <AuthProvider>
-        <Routes>
-          <Route element={<PrivateRoute />}>
-            <Route path="/dashboard/*" element={<DashboardRoutes />} />
-          </Route>
+        <NotifyProvider>
+          <Routes>
+            <Route element={<PrivateRoute />}>
+              <Route path="/dashboard/*" element={<DashboardRoutes />} />
+            </Route>
 
-          <Route path="/*" element={<HomeRoutes />} />
-        </Routes>
+            <Route path="/*" element={<HomeRoutes />} />
+          </Routes>
+        </NotifyProvider>
       </AuthProvider>
 
       <Footer />
